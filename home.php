8109cf8c0ea41a397b7c2ecef2fe7ec1de3eb44f
rinuaby13 <47815898+rinuaby13@users.noreply.github.com>
2021-09-23 17:45:24 +0530

    Add files via upload

---
+++ home.php
@@ -0,0 +1,36 @@
+<!-- <h1><?php echo $title;?></h1> -->
+
+<div class="col-lg-12">  
+<h4>E-learning</h4>
+    <p> Online courses are the biggest saviours of students and professionals today. From guitar classes to learning school subjects, online tuitions have brought revolutionary modifications in the way education was disseminated. Ever wondered how this brilliant idea of the online study came on the surface?  </p> 
+</div>
+<div  class="col-lg-6">
+  
+  <p>Historically, online tutoring began with an email. In this format, a student sent a question to the tutor with the expectation that the return email would contain “the answer.” Instead, what happenwas a disconnection: The tutor, being a good guide, sent back a Socratic answer with more questioning prompts; the student, expecting “the answer,” became frustrated. Although the student may expect a give and take interaction in a face-to-face tutoring session, the email format suggested to the student that the question should be answered with a direct answer.
+</p>
+</div>
+
+<!--
+  <h4>Mission</h4>
+  <p> The Philippine Science High School, operating under one System of Governance and Management, provides scholarship to students with high aptitude in science and mathematics.</p>
+  <h4>Core Values</h4> 
+  <ol>
+    <li>Pursuit of TRUTH.</li>  
+    <li>Passion for EXCELLENCE</li> 
+    <li>Commitment to SERVICE</li> 
+  </ol>
+
+
+</div>
+<div  class="col-lg-6">
+  <h4>Guiding Principles</h4> 
+    <ul>
+      <li>Academic Freedom </li>
+      <li>Responsibility</li>
+      <li>Academic Standards</li>  
+    </ul>
+
+</div>
+-->
+ 
+
